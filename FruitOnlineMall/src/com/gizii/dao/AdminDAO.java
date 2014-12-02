f2907c80bfdeeb4e7d7485f2da93271113b0401c
empty-sq <865329347@qq.com>
2014-12-02 21:48:27 +0800

    because cld

--- FruitOnlineMall/src/com/gizii/dao/AdminDAO.java
+++ FruitOnlineMall/src/com/gizii/dao/AdminDAO.java
@@ -7,6 +7,7 @@ import com.gizii.po.Admin;
 public interface AdminDAO {
 	public void add(Admin admin);
 	public void delete(int Id);
+	public void cld(int xx);
 	public void updata(Admin admin);
 	public Admin showById(int Id);
 	public Admin getByName(String username);
