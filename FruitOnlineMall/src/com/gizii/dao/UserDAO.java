d0e5e0ac3cf8879e89d0e0744ba19dd7950c72db
CLD1994 <772427859@qq.com>
2015-01-30 20:43:21 +0800

    删除旧版

--- FruitOnlineMall/src/com/gizii/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.gizii.dao;
-
-import com.gizii.po.User;
-
-public interface UserDAO {
-	public void add(User user);
-	
-	public void deleteAll();
-}
--- FruitOnlineMall/src/com/gizii/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gizii.mapper;
-
-import com.gizii.po.User;
-
-public interface UserMapper {
-	public void add(User user);
-	
-	public void deleteAll();
-}
